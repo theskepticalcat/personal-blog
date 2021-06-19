6718b33a226ce22824c47cf2c987a354880da760
theskepticalcat <49359333+theskepticalcat@users.noreply.github.com>
2021-06-19 22:55:21 +0800

    logic for textareas

---
+++ src/assets/js/textarea.js
@@ -0,0 +1,12 @@
+const textArea = document.querySelectorAll('[data-autoresize]');
+
+textArea.forEach(item => {
+    let textAreaHeigth = item.offsetHeight;  //получаем высоту текстового поля заданную по умолчанию
+
+    item.addEventListener('input', event => {
+        let $this = event.target;
+
+        $this.style.height = textAreaHeigth + 'px';
+        $this.style.height = $this.scrollHeight + 'px';
+    });
+});
